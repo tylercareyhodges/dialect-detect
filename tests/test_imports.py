74f921dd665d7298d21462a1d62f21668a94d58f
Tyler Carey-Hodges <tylercareyhodges@gmail.com>
2025-09-22 17:28:13 +0100

    Update test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,1 +1,8 @@
+def test_imports():
+    import src.datasets as _  # noqa
+    import src.features as _  # noqa
+    import src.model as _     # noqa
+    import src.train as _     # noqa
+    import src.eval as _      # noqa
+    import src.inference as _ # noqa
 
